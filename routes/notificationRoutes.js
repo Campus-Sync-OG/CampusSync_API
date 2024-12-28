c261bfd0a1983f63c0e8e7b044fd7be1230860ad
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2024-12-28 15:55:28 +0530

    dumped all old database

--- routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-
-// Route to send notifications
-router.post('/notify', notificationController.sendNotification);
-
-module.exports = router;
