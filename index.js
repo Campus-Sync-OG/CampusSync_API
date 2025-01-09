48c72dd6f5657f21ca5255fc0aef780bf6c98c9d
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-01-09 11:27:44 +0530

    new database is created

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const UserRoutes = require("./routes/UserRoutes");
-const studentRoutes = require('./routes/StudentRoutes'); 
-const AttendanceRoutes=require("./routes/AttendanceRoutes");
+
 
 
 app.use(express.json());
@@ -14,9 +12,7 @@ PORT = process.env.PORT||3000;
 app.get("/", (req, res) => {
   res.send("Welcome to admin panel");
 });
-app.use("/api", UserRoutes);
-app.use('/api', studentRoutes);
-app.use("/api/attendance",AttendanceRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on Port: ${PORT}`);
 });
