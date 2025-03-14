944b3f4e98ca498ea9e43dbcd401667d88298044
Ritika08092001 <ritikanetalkar@gmail.com>
2025-03-14 18:58:23 +0530

    fee checked

--- routes/FeeRoutes.js
+++ routes/FeeRoutes.js
@@ -4,11 +4,11 @@ const feeController = require("../controllers/FeeController");
 const Auth = require("../middleware/authMiddleware");
 
 // Get Fees by Admission No
- router.get("/:admission_no", Auth.verifyToken, feeController.getFeesByAdmissionNo);
+ router.get("/getbyid/:admission_no",Auth.verifyToken, feeController.getFeesByAdmissionNo);
 
 // Generate and Download Fee PDF
-router.get('/download-fee/:receipt_no', Auth.verifyToken, feeController.downloadFeePDF);
-router.get('/getfees',Auth.verifyToken, feeController.getAllFees);
+router.get('/download-fee/:receipt_no', feeController.downloadFeePDF);
+router.get('/getfee',Auth.verifyToken, feeController.getAllFees);
 router.delete('/:id',Auth.verifyToken, feeController.deleteFee);
 
 module.exports = router;
