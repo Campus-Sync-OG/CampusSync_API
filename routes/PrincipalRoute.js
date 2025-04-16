0bc575f1948bc3f5721ceeae627f327592aab50c
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-04-16 11:43:57 +0530

    removed the principal routes

--- routes/PrincipalRoute.js
+++ routes/PrincipalRoute.js
@@ -7,9 +7,6 @@ router.post('/create', Auth.verifyToken, principalController.createPrincipal);
 router.put('/update/:p_id', Auth.verifyToken, principalController.updatePrincipal);
 router.delete('/delete/:p_id', Auth.verifyToken, principalController.softDeletePrincipal);
 router.get('/:p_id', Auth.verifyToken, principalController.getPrincipalDetails);
-router.post('/:p_id/subjects', Auth.verifyToken, principalController.createSubject);
-router.put("/:p_id/update/subjects/:id", Auth.verifyToken, principalController.updateSubject);
-
-//get feedback
 router.get("/view",Auth.verifyToken, principalController.getAllFeedback);
+
 module.exports = router;
