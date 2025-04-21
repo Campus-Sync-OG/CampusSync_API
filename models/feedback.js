23e5b950824b34d236979293108264198fe16153
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-04-21 10:21:46 +0530

    changed table name of feedback

--- models/feedback.js
+++ models/feedback.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
           allowNull: false
       },
   }, {
-      tableName: "feedbacks",
+      tableName: "feedback",
       timestamps: true
   });
 
