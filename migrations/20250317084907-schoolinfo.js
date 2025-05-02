4f3bd47a50459fc7f39a95cbbebd76d82afdc817
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-05-02 11:18:51 +0530

    chnged modelname of scholinfo

--- migrations/20250317084907-schoolinfo.js
+++ migrations/20250317084907-schoolinfo.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Shoolinfo', {
+    await queryInterface.createTable('schoolinfo', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
