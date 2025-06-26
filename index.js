c2eede6b9c7f46ef017c281851fd2d6b53c87f75
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-06-26 13:08:27 +0530

    updated the cors endpoints

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "https://school-test.trippcard.in/"
+     
     ],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true
