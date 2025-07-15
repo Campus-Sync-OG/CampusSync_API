80b3848a6914874bdb9aecb126191f3e2a41f894
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-07-15 15:19:28 +0530

    updated academics migration file

--- migrations/20250110172834-academics-table.js
+++ migrations/20250110172834-academics-table.js
@@ -1,30 +1,51 @@
+'use strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('academics', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
       admission_no: {
-        type: Sequelize.STRING(10),
+        type: Sequelize.STRING,
         allowNull: false,
         references: {
-          model: 'student', // Table name
+          model: 'student',
           key: 'admission_no',
         },
+        onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       },
-      teacher_name: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      subject: {
+      subjects: {
         type: Sequelize.STRING,
         allowNull: false,
+        references: {
+          model: 'subject',
+          key: 'subject_name',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
       class_grade: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      term_semester: {
+      section: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      exam_format: {
         type: Sequelize.STRING,
         allowNull: false,
+        references: {
+          model: 'examformat',
+          key: 'exam_name',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
       academic_year: {
         type: Sequelize.STRING,
@@ -41,11 +62,11 @@ module.exports = {
       exam_date: {
         type: Sequelize.DATE,
         allowNull: true,
-      },
+      }
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('academics');
-  },
+  }
 };
